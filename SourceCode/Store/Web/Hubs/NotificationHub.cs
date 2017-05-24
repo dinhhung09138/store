9e9f3a2d35fa8741d946f71587bfe218b980d997
hung.td <hung.td@aris-vn.com>
2017-05-24 16:35:58 +0700

    demo signalr

--- SourceCode/Store/Web/Hubs/NotificationHub.cs
+++ SourceCode/Store/Web/Hubs/NotificationHub.cs
@@ -4,18 +4,21 @@ using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
 using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Hubs;
 
 namespace Web.Hubs
 {
+    [HubName("NotificationHub")]
     public class NotificationHub : Hub
     {
         public override Task OnConnected()
         {
-            string name = Context.User.Identity.Name;
-            Groups.Add(Context.ConnectionId, name);
+            //string name = Context.User.Identity.Name;
+            //Groups.Add(Context.ConnectionId, name);
 
             return base.OnConnected();
         }
+
 
         public void Hello()
         {
@@ -32,10 +35,134 @@ namespace Web.Hubs
             Clients.All.addNewMessageToPage(name, message);
         }
 
-        public void Send(string name, string message, string userName)
+        //public void Send(string name, string message, string userName)
+        //{
+        //    // Call the addNewMessageToPage method to update clients.
+        //    Clients.All.addNewMessageToPage(name, message);
+        //}
+
+        public void Send(string name, string message, string roomName)
         {
             // Call the addNewMessageToPage method to update clients.
-            Clients.All.addNewMessageToPage(name, message);
+            //await Groups.Add(name, roomName);
+            Clients.Group(roomName).addNewMessageToPage("", name + " joined");
+            //Clients.OthersInGroup(roomName).addNewMessageToPage("", name + " joined");
+            Clients.All.addNewMessageToPage(name, message + " " + Context.ConnectionId);
         }
+
+        public void JoinRoom(string userName, string roomName)
+        {
+            Groups.Add(Context.ConnectionId, roomName);
+            Clients.Group(roomName).addNewMessageToPage("", userName + " joined");
+        }
+
+        public Task LeaveRoom(string roomName)
+        {
+            return Groups.Remove(Context.ConnectionId, roomName);
+        }
+    }
+    
+    public class ChatHub : Hub
+    {
+        #region Data Members
+
+        static List<UserDetail> ConnectedUsers = new List<UserDetail>();
+        static List<MessageDetail> CurrentMessage = new List<MessageDetail>();
+
+        #endregion
+
+        #region Methods
+
+        public void Connect(string userName)
+        {
+            var id = Context.ConnectionId;
+
+
+            if (ConnectedUsers.Count(x => x.ConnectionId == id) == 0)
+            {
+                ConnectedUsers.Add(new UserDetail { ConnectionId = id, UserName = userName });
+
+                // send to caller
+                Clients.Caller.onConnected(id, userName, ConnectedUsers, CurrentMessage);
+
+                // send to all except caller client
+                Clients.AllExcept(id).onNewUserConnected(id, userName);
+
+            }
+
+        }
+
+        public void SendMessageToAll(string userName, string message)
+        {
+            // store last 100 messages in cache
+            AddMessageinCache(userName, message);
+
+            // Broad cast message
+            Clients.All.messageReceived(userName, message);
+        }
+
+        public void SendPrivateMessage(string toUserId, string message)
+        {
+
+            string fromUserId = Context.ConnectionId;
+
+            var toUser = ConnectedUsers.FirstOrDefault(x => x.ConnectionId == toUserId);
+            var fromUser = ConnectedUsers.FirstOrDefault(x => x.ConnectionId == fromUserId);
+
+            if (toUser != null && fromUser != null)
+            {
+                // send to 
+                Clients.Client(toUserId).sendPrivateMessage(fromUserId, fromUser.UserName, message);
+
+                // send to caller user
+                Clients.Caller.sendPrivateMessage(toUserId, fromUser.UserName, message);
+            }
+
+        }
+
+        //public override System.Threading.Tasks.Task OnDisconnected()
+        //{
+        //    var item = ConnectedUsers.FirstOrDefault(x => x.ConnectionId == Context.ConnectionId);
+        //    if (item != null)
+        //    {
+        //        ConnectedUsers.Remove(item);
+
+        //        var id = Context.ConnectionId;
+        //        Clients.All.onUserDisconnected(id, item.UserName);
+
+        //    }
+
+        //    return base.OnDisconnected();
+        //}
+
+
+        #endregion
+
+        #region private Messages
+
+        private void AddMessageinCache(string userName, string message)
+        {
+            CurrentMessage.Add(new MessageDetail { UserName = userName, Message = message });
+
+            if (CurrentMessage.Count > 100)
+                CurrentMessage.RemoveAt(0);
+        }
+
+        #endregion
+    }
+
+    public class UserDetail
+    {
+        public string ConnectionId { get; set; }
+        public string UserName { get; set; }
+    }
+
+    public class MessageDetail
+    {
+
+        public string UserName { get; set; }
+
+        public string Message { get; set; }
+
     }
 }
