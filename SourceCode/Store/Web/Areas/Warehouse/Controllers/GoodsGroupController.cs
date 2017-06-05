c3535f6399da702ded38b8e83c0f3f0ce7d9d9fd
HUNGTD-PC\hungtd <dinhhung09138@gmail.com>
2017-06-05 07:07:28 +0700

    add unit and group of goods form

---
+++ SourceCode/Store/Web/Areas/Warehouse/Controllers/GoodsGroupController.cs
@@ -0,0 +1,76 @@
+﻿using Common.JqueryDataTable;
+using Common.Status;
+using DataAccess;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web.Filters;
+
+namespace Web.Areas.Warehouse.Controllers
+{
+    public class GoodsGroupController : Controller
+    {
+        [CustomAuthorize]
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [AjaxAuthorize]
+        [HttpPost]
+        public JsonResult Index(CustomJqueryDataTableRequest requestData)
+        {
+            requestData = requestData.SetOrderingColumnName();
+            GoodsGroupSrv _srvGroup = new GoodsGroupSrv();
+            Dictionary<string, object> _return = _srvGroup.List(requestData);
+            if ((DatabaseExecute)_return["status"] == DatabaseExecute.Success)
+            {
+                JqueryDataTableResponse<GoodsGroupModel> itemResponse = _return["data"] as JqueryDataTableResponse<GoodsGroupModel>;
+                return this.Json(itemResponse, JsonRequestBehavior.AllowGet);
+            }
+            return this.Json(new JqueryDataTableResponse<GoodsGroupModel>(), JsonRequestBehavior.AllowGet);
+        }
+
+        [AjaxAuthorize]
+        [HttpGet]
+        public ActionResult Add()
+        {
+            GoodsGroupModel model = new GoodsGroupModel();
+            model.Insert = true;
+            return PartialView(model);
+        }
+
+        [AjaxAuthorize]
+        [HttpGet]
+        public ActionResult Edit(string id)
+        {
+            GoodsGroupSrv _srvGroup = new GoodsGroupSrv();
+            GoodsGroupModel model = _srvGroup.Item(new Guid(id));
+            model.Insert = false;
+            return PartialView(model);
+        }
+
+        [AjaxAuthorize]
+        [HttpPost]
+        public JsonResult Save(GoodsGroupModel model)
+        {
+            Model.User.UserLoginModel user = Session["user"] as Model.User.UserLoginModel;
+            if (model.Insert)
+            {
+                model.ID = Guid.NewGuid();
+                model.CreateBy = user.ID;
+            }
+            else
+            {
+                model.UpdatedBy = user.ID;
+            }
+            GoodsGroupSrv _srvGroup = new GoodsGroupSrv();
+            return this.Json(_srvGroup.Save(model), JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ SourceCode/Store/Web/Areas/Warehouse/Controllers/UnitController.cs
@@ -0,0 +1,76 @@
+﻿using Common.JqueryDataTable;
+using Common.Status;
+using DataAccess;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web.Filters;
+
+namespace Web.Areas.Warehouse.Controllers
+{
+    public class UnitController : Controller
+    {
+        [CustomAuthorize]
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [AjaxAuthorize]
+        [HttpPost]
+        public JsonResult Index(CustomJqueryDataTableRequest requestData)
+        {
+            requestData = requestData.SetOrderingColumnName();
+            UnitSrv _srvUnit = new UnitSrv();
+            Dictionary<string, object> _return = _srvUnit.List(requestData);
+            if ((DatabaseExecute)_return["status"] == DatabaseExecute.Success)
+            {
+                JqueryDataTableResponse<UnitModel> itemResponse = _return["data"] as JqueryDataTableResponse<UnitModel>;
+                return this.Json(itemResponse, JsonRequestBehavior.AllowGet);
+            }
+            return this.Json(new JqueryDataTableResponse<UnitModel>(), JsonRequestBehavior.AllowGet);
+        }
+
+        [AjaxAuthorize]
+        [HttpGet]
+        public ActionResult Add()
+        {
+            UnitModel model = new UnitModel();
+            model.Insert = true;
+            return PartialView(model);
+        }
+
+        [AjaxAuthorize]
+        [HttpGet]
+        public ActionResult Edit(string id)
+        {
+            UnitSrv _srvUnit = new UnitSrv();
+            UnitModel model = _srvUnit.Item(new Guid(id));
+            model.Insert = false;
+            return PartialView(model);
+        }
+
+        [AjaxAuthorize]
+        [HttpPost]
+        public JsonResult Save(UnitModel model)
+        {
+            Model.User.UserLoginModel user = Session["user"] as Model.User.UserLoginModel;
+            if (model.Insert)
+            {
+                model.ID = Guid.NewGuid();
+                model.CreateBy = user.ID;
+            }
+            else
+            {
+                model.UpdatedBy = user.ID;
+            }
+            UnitSrv _srvUnit = new UnitSrv();
+            return this.Json(_srvUnit.Save(model), JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
